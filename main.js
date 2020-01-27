ca30705be3086d77eb0b27fd621dcf3576df0105
RockyKev <kev.rocky@gmail.com>
2020-01-26 20:16:29 -0800

    last update

--- main.js
+++ main.js
@@ -3,20 +3,39 @@
 function init() {
   const scene = new THREE.Scene();
 
+  const box = getBox(1, 1, 1);
+
+  scene.add(box);
+
   const camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
     1,
     1000
   );
+  camera.position.x = 1;
+  camera.position.y = 2;
+  camera.position.z = 5;
+
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
   const renderer = new THREE.WebGLRenderer();
-
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   document.getElementById("webgl").appendChild(renderer.domElement);
-
   renderer.render(scene, camera);
 }
 
 init();
+
+function getBox(w, h, d) {
+  //mesh (shape and material) Default is Mesh, which has no reflection on lighting
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+  });
+
+  const mesh = new THREE.Mesh(geometry, material);
+
+  return mesh;
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Lynda.com -> Three.js
+
+https://www.lynda.com/JavaScript-tutorials/Create-ground-plane/586668/633294-4.html?srchtrk=index%3a1%0alinktypeid%3a2%0aq%3athreejs%0apage%3a1%0as%3arelevance%0asa%3atrue%0aproducttypeid%3a2
